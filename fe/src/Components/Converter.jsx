387f0e42cb325e87a96f7c7f84f8cb31e2a1ecab
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-30 00:42:28 +0530

    aa

--- fe/src/Components/Converter.jsx
+++ fe/src/Components/Converter.jsx
@@ -1,37 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Converter.css";
+import axios from "axios";
 export default function Converter() {
+  const [textarea, setTextArea] = useState("");
+  const [language, setlanguage] = useState("");
+  const [data, setData] = useState("");
+  const [loading, setloading] = useState(false);
+  const handleconvert = () => {
+    let obj = {
+      textarea,
+      language,
+    };
+    setloading(true);
+
+    fetch(`https://thoughtful-ox-train.cyclic.app/convertcode`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: textarea, language: language }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setloading(false);
+        console.log(data.codes);
+        setData(data.codes);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handledebug = () => {
+    setloading(true);
+
+    fetch(`https://thoughtful-ox-train.cyclic.app/debugcode`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: textarea, language: language }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setloading(false);
+        console.log(data);
+        setData(data.response);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handlecheckqty = () => {
+    setloading(true);
+
+    fetch(`https://thoughtful-ox-train.cyclic.app/checkquality`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: textarea, language: language }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setloading(false);
+        console.log(data);
+        setData(data.response);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div>
-      <div class="container">
+      <div className="container">
         <h1>Code Converter</h1>
-        <div class="form">
+        <div className="form">
           <textarea
+            onChange={(e) => setTextArea(e.target.value)}
             id="codeInput"
             placeholder="Enter your code here"
           ></textarea>
-          <select id="languageSelect">
-            <option value="javascript">C++</option>
-            <option value="javascript">C#</option>
+          <select
+            id="languageSelect"
+            onChange={(e) => setlanguage(e.target.value)}
+          >
+            <option value="c++">C++</option>
+            <option value="c#">C#</option>
             <option value="java">Java</option>
             <option value="python">Python</option>
             <option value="javascript">JavaScript</option>
-            <option value="javascript">TypeScript</option>
+            <option value="typescript">TypeScript</option>
           </select>
-          <div class="buttons">
-            <button id="convertButton" onclick="convertCode()">
+          <div className="buttons">
+            <button id="convertButton" onClick={handleconvert}>
               Convert
             </button>
-            <button id="clearButton" onclick="clearCode()">
+            <button id="clearButton" onClick={handledebug}>
               Debug
             </button>
-            <button id="copyButton" onclick="copyCode()">
+            <button id="copyButton" onClick={handlecheckqty}>
+              {" "}
               Check Quality
             </button>
           </div>
         </div>
-        <div id="output" class="output">
-          <pre id="convertedCode"></pre>
+        <div id="output" className="output">
+          {loading ? <h3>Loading.....</h3> : <h4 id="convertedCode">{data}</h4>}
         </div>
       </div>
     </div>
