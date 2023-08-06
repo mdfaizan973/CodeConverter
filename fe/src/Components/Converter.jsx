cbed2fec813d614be95027dabaccb069fac03a1d
mdfaizan973 <faizan.md9735@gmail.com>
2023-08-06 22:05:24 +0530

    update

--- fe/src/Components/Converter.jsx
+++ fe/src/Components/Converter.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import "./Converter.css";
 import axios from "axios";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Textarea,
+  Select,
+  Divider,
+  VStack,
+  CircularProgress,
+  chakra,
+} from "@chakra-ui/react";
 export default function Converter() {
   const [textarea, setTextArea] = useState("");
   const [language, setlanguage] = useState("");
@@ -74,43 +86,97 @@ export default function Converter() {
   };
 
   return (
-    <div>
-      <div className="container">
-        <h1>Code Converter</h1>
-        <div className="form">
-          <textarea
-            onChange={(e) => setTextArea(e.target.value)}
-            id="codeInput"
-            placeholder="Enter your code here"
-          ></textarea>
-          <select
-            id="languageSelect"
+    // <div>
+    //   <div className="container">
+    //     <h1>Code Converter</h1>
+    //     <div className="form">
+    //       <textarea
+    //         onChange={(e) => setTextArea(e.target.value)}
+    //         id="codeInput"
+    //         placeholder="Enter your code here"
+    //       ></textarea>
+    //       <select
+    //         id="languageSelect"
+    //         onChange={(e) => setlanguage(e.target.value)}
+    //       >
+    //         <option value="c++">C++</option>
+    //         <option value="c#">C#</option>
+    //         <option value="java">Java</option>
+    //         <option value="python">Python</option>
+    //         <option value="javascript">JavaScript</option>
+    //         <option value="typescript">TypeScript</option>
+    //       </select>
+    //       <div className="buttons">
+    //         <button id="convertButton" onClick={handleconvert}>
+    //           Convert
+    //         </button>
+    //         <button id="clearButton" onClick={handledebug}>
+    //           Debug
+    //         </button>
+    //         <button id="copyButton" onClick={handlecheckqty}>
+    //           {" "}
+    //           Check Quality
+    //         </button>
+    //       </div>
+    //     </div>
+    //     <div id="output" className="output">
+    //       {loading ? <h3>Loading.....</h3> : <h4 id="convertedCode">{data}</h4>}
+    //     </div>
+    //   </div>
+    // </div>
+    <div id="main_container">
+      <Container maxW="lg" py={10}>
+        <VStack spacing={6} align="center">
+          <Heading size="xl">Code Converter</Heading>
+          <Box w="full">
+            <Textarea
+              value={textarea}
+              onChange={(e) => setTextArea(e.target.value)}
+              placeholder="Enter your code here"
+              size="md"
+            />
+          </Box>
+          <Select
+            value={language}
             onChange={(e) => setlanguage(e.target.value)}
+            placeholder="Select a language"
+            size="md"
           >
             <option value="c++">C++</option>
             <option value="c#">C#</option>
             <option value="java">Java</option>
             <option value="python">Python</option>
+            <option value="python">Rust</option>
+            <option value="python">Php</option>
+            <option value="python">Ruby</option>
             <option value="javascript">JavaScript</option>
             <option value="typescript">TypeScript</option>
-          </select>
-          <div className="buttons">
-            <button id="convertButton" onClick={handleconvert}>
+          </Select>
+          <VStack spacing={4}>
+            <Button colorScheme="blue" onClick={handleconvert}>
               Convert
-            </button>
-            <button id="clearButton" onClick={handledebug}>
+            </Button>
+            <Button colorScheme="red" onClick={handledebug}>
               Debug
-            </button>
-            <button id="copyButton" onClick={handlecheckqty}>
-              {" "}
+            </Button>
+            <Button colorScheme="green" onClick={handlecheckqty}>
               Check Quality
-            </button>
-          </div>
-        </div>
-        <div id="output" className="output">
-          {loading ? <h3>Loading.....</h3> : <h4 id="convertedCode">{data}</h4>}
-        </div>
-      </div>
+            </Button>
+          </VStack>
+          <Divider />
+          <Box w="full">
+            <Box p={4} border="1px solid gray" borderRadius="md">
+              {loading ? (
+                <CircularProgress isIndeterminate color="blue.500" />
+              ) : (
+                <chakra.h4 whiteSpace="pre-line" fontSize="md">
+                  {data}
+                </chakra.h4>
+              )}
+            </Box>
+          </Box>
+        </VStack>
+      </Container>
     </div>
   );
 }
